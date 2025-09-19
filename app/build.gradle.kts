2ca803f222d3ec56e376809d021df12ec765307f
Joalisson da Costa Moreira <63914002+joalissoncm@users.noreply.github.com>
2025-09-18 22:20:04 -0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm) // <1>
+    application // <2>
+}
+
+repositories {
+    mavenCentral() // <3>
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test") // <4>
+
+    testImplementation(libs.junit.jupiter.engine) // <5>
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava) // <6>
+}
+
+application {
+    mainClass = "demo.AppKt" // <7>
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform() // <8>
+}
